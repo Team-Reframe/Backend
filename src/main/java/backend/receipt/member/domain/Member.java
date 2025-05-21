42209f9be88e052d3e5216c29f0b919c3f0126d8
maxdev-lab <zenon789@naver.com>
2025-05-22 01:09:42 +0900

    feat: 엔티티 정의 변경

--- src/main/java/backend/receipt/member/domain/Member.java
+++ src/main/java/backend/receipt/member/domain/Member.java
@@ -21,8 +21,8 @@ public class Member {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, unique = true)
-    private String name;
+    @Column(nullable = false)
+    private String name; //실명
 
     public Member(String email, String password, String name) {
         this.email = email;
